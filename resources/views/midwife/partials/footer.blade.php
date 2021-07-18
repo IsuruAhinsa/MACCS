59125ed0607687690048754fca9112d0cab8df4a
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-18 15:50:51 +0530

    midwife partials created

---
+++ resources/views/midwife/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer id="page-footer" class="bg-body-light">
+    <div class="content py-3">
+        <div class="row font-size-sm">
+            <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-left">
+                <a class="font-w600" href="{{ url('/') }}" target="_blank">{{ config('app.name') }}</a> &copy; <span data-toggle="year-copy">2020</span>
+            </div>
+        </div>
+    </div>
+</footer>
