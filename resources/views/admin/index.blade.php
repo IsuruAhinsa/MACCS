5929bdea354b23f25e0ff4c23075f1b8e2c7151a
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-18 15:49:48 +0530

    custom dashboard created for admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @include('admin.partials.sidebar')
+
+    @include('admin.partials.header')
+
+    <main id="main-container">
+
+        <!-- Hero -->
+        <div class="bg-image overflow-hidden" style="background-image: url('assets/media/photos/photo3@2x.jpg');">
+            <div class="bg-primary-dark-op">
+                <div class="content content-narrow content-full">
+                    <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center mt-5 mb-2 text-center text-sm-left">
+                        <div class="flex-sm-fill">
+                            <h1 class="font-w600 text-white mb-0 invisible" data-toggle="appear">{{ config('app.name') }} Dashboard</h1>
+                            <h2 class="h4 font-w400 text-white-75 mb-0 invisible" data-toggle="appear" data-timeout="250">Welcome Administrator</h2>
+                        </div>
+                        <div class="flex-sm-00-auto mt-3 mt-sm-0 ml-sm-3">
+                        <span class="d-inline-block invisible" data-toggle="appear" data-timeout="350">
+                            <a class="btn btn-primary px-4 py-2" data-toggle="click-ripple" href="#">
+                                <i class="fa fa-plus mr-1"></i> New Midwife
+                            </a>
+
+                            <a class="btn btn-success px-4 py-2" data-toggle="click-ripple" href="#">
+                                <i class="fa fa-plus mr-1"></i> New Child
+                            </a>
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END Hero -->
+
+        <!-- Page Content -->
+        <div class="content content-narrow">
+            <!-- Stats -->
+            <div class="row">
+                <div class="col-6 col-md-3 col-lg-6 col-xl-3">
+                    <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                        <div class="block-content block-content-full">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">Children</div>
+                            <div class="font-size-h2 font-w400 text-dark">580</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-6 col-md-3 col-lg-6 col-xl-3">
+                    <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                        <div class="block-content block-content-full">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">Midwives</div>
+                            <div class="font-size-h2 font-w400 text-dark">150</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-6 col-md-3 col-lg-6 col-xl-3">
+                    <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                        <div class="block-content block-content-full">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">Parents</div>
+                            <div class="font-size-h2 font-w400 text-dark">200</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-6 col-md-3 col-lg-6 col-xl-3">
+                    <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                        <div class="block-content block-content-full">
+                            <div class="font-size-sm font-w600 text-uppercase text-muted">Users</div>
+                            <div class="font-size-h2 font-w400 text-dark">21</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- END Stats -->
+        </div>
+        <!-- END Page Content -->
+
+    </main>
+
+    @include('admin.partials.footer')
+
+@endsection
