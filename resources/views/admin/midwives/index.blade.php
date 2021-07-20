fca3c4c512384240758dba26897af12082bc0595
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-20 09:27:43 +0530

    All midwives updated

--- resources/views/admin/midwives/index.blade.php
+++ resources/views/admin/midwives/index.blade.php
@@ -29,83 +29,78 @@
                 <div class="row row-deck">
                     <!-- Latest Customers -->
                     <div class="col-lg-12">
-                        <div class="block block-mode-loading-oneui">
+                        <div class="block block-mode-loading-oneui block-themed">
                             <div class="block-header border-bottom">
                                 <h3 class="block-title">All Midwives</h3>
-                                <div class="block-options">
-                                    <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle" data-action-mode="demo">
-                                        <i class="si si-refresh"></i>
-                                    </button>
-                                    <button type="button" class="btn-block-option">
-                                        <i class="si si-settings"></i>
-                                    </button>
-                                </div>
                             </div>
                             <div class="block-content block-content-full">
-                                <table class="table table-striped table-hover table-borderless table-vcenter font-size-sm mb-0">
-                                    <thead class="thead-dark">
-                                    <tr class="text-uppercase">
-                                        <th class="font-w700" style="width: 80px;">ID</th>
-                                        <th class="d-none d-sm-table-cell font-w700 text-center" style="width: 100px;">Photo</th>
-                                        <th class="font-w700">First Name</th>
-                                        <th class="font-w700">Last Name</th>
-                                        <th class="font-w700">Phone</th>
-                                        <th class="font-w700">Email</th>
-                                        <th class="font-w700">Created At</th>
-                                        <th class="d-none d-sm-table-cell font-w700 text-center" style="width: 80px;">Cases</th>
-                                        <th class="font-w700 text-center" style="width: 60px;">Action</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($recorders as $midwife)
-                                    <tr>
-                                        <td>
-                                            <span class="font-w600">#</span>
-                                        </td>
-                                        <td class="d-none d-sm-table-cell text-center">
-                                            <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
-                                        </td>
-                                        <td class="font-w600">
-                                            {{$midwife->first_name}}
-                                        </td>
-                                        <td class="font-w600">
-                                            {{$midwife->last_name}}
-                                        </td>
-                                        <td class="font-w600">
-                                            {{$midwife->phone}}
-                                        </td>
-                                        <td class="font-w600">
-                                            {{$midwife->email}}
-                                        </td>
-                                        <td class="font-w600">
-                                            {{$midwife->created_at}}
-                                        </td>
-                                        <td class="d-none d-sm-table-cell text-center">
-                                            <a class="link-fx font-w600" href="javascript:void(0)">{{$midwife->cases}}</a>
-                                        </td>
-                                        <td class="text-center">
-                                            <a href="javascript:void(0)" data-toggle="tooltip" data-placement="left" title="Edit">
-                                                <i class="fa fa-fw fa-pencil-alt"></i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-hover table-borderless table-vcenter font-size-sm mb-0">
+                                        <thead class="thead-dark">
+                                        <tr class="text-uppercase">
+                                            <th class="font-w700" style="width: 80px;">ID</th>
+                                            <th class="d-none d-sm-table-cell font-w700 text-center" style="width: 100px;">Photo</th>
+                                            <th class="font-w700">First Name</th>
+                                            <th class="font-w700">Last Name</th>
+                                            <th class="font-w700">Phone</th>
+                                            <th class="font-w700">Email</th>
+                                            <th class="font-w700">Created At</th>
+                                            <th class="d-none d-sm-table-cell font-w700 text-center" style="width: 80px;">Cases</th>
+                                            <th class="font-w700 text-center" style="width: 60px;">Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($recorders as $midwife)
+                                            <tr>
+                                                <td>
+                                                    <span class="font-w600">
+                                                        {{ $loop->index + 1 }}
+                                                    </span>
+                                                </td>
+                                                <td class="d-none d-sm-table-cell text-center">
+                                                    <img class="img-avatar img-avatar32" src="assets/media/avatars/avatar15.jpg" alt="">
+                                                </td>
+                                                <td class="font-w600">
+                                                    {{$midwife->first_name}}
+                                                </td>
+                                                <td class="font-w600">
+                                                    {{$midwife->last_name}}
+                                                </td>
+                                                <td class="font-w600">
+                                                    {{$midwife->phone}}
+                                                </td>
+                                                <td class="font-w600">
+                                                    {{$midwife->email}}
+                                                </td>
+                                                <td class="font-w600">
+                                                    {{$midwife->created_at}}
+                                                </td>
+                                                <td class="font-w600 text-center">
+                                                    {{$midwife->cases}}
+                                                </td>
+                                                <td class="text-center">
+                                                    <a href="{{ route('midwives.edit', $midwife->id) }}" data-toggle="tooltip" data-placement="left" title="Edit">
+                                                        <i class="fa fa-fw fa-pencil-alt"></i>
+                                                    </a>
+                                                    <a href="{{ route('midwives.destroy', $midwife->id) }}" data-toggle="tooltip" data-placement="left" title="Delete">
+                                                        <i class="fa fa-fw fa-trash text-danger"></i>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
 
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <!-- END Latest Customers -->
+
                 </div>
 
             </div>
         </div>
 
     </div>
-
-
-
-
 
 @endsection
