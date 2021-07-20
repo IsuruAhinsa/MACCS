7f96de87a28833f99134aab7012c3690b27229c2
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-20 20:40:27 +0530

    Admin Create Function Started

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -21,11 +21,10 @@ class AdminController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('admin.admin.create');
     }
 
     /**
