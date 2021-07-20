f55d88a61ff3bf0ade3f2526e6becc564b303375
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-20 19:31:56 +0530

    migration files restructured

--- database/migrations/2021_07_20_135937_create_users_table.php
+++ database/migrations/2021_07_20_135937_create_users_table.php
@@ -15,16 +15,26 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('midwife_id');
+            $table->foreign('midwife_id')->references('id')->on('midwives')->onDelete('cascade');
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('postal')->nullable();
+            $table->string('image')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('nic')->nullable();
+            $table->string('type')->nullable();
+            $table->integer('children')->nullable();
             $table->timestamp('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
