445061ad200513f557df425896305c04ebcfd817
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-07-20 20:27:20 +0530

    midwife create.blade.php and midwife edit.blade.php updated

--- resources/views/admin/midwives/edit.blade.php
+++ resources/views/admin/midwives/edit.blade.php
@@ -50,11 +50,16 @@
                                 <label for="first_name" class="col-form-label">First Name</label>
                                 <input
                                     type="text"
-                                    class="form-control form-control-alt"
+                                    class="form-control form-control-alt @error('first_name') is-invalid @enderror"
                                     id="first_name"
                                     placeholder="Enter First Name"
                                     name="first_name"
                                     value="{{$midwife->first_name}}"
+                                @error('first_name')
+                                <small class="invalid-feedback">
+                                    {{ $message }}
+                                </small>
+                                @enderror
                                 >
                             </div>
 
@@ -62,24 +67,34 @@
                                 <label for="last_name" class="col-form-label">Last Name</label>
                                 <input
                                     type="text"
-                                    class="form-control form-control-alt"
+                                    class="form-control form-control-alt @error('last_name') is-invalid @enderror"
                                     id="last_name"
                                     placeholder="Enter Last Name"
                                     name="last_name"
                                     value="{{$midwife->last_name}}"
                                 >
+                                @error('last_name')
+                                <small class="invalid-feedback">
+                                    {{ $message }}
+                                </small>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="email" class="col-form-label">Email</label>
                                 <input
                                     type="email"
-                                    class="form-control form-control-alt"
+                                    class="form-control form-control-alt @error('email') is-invalid @enderror"
                                     id="email"
                                     placeholder="Enter Email"
                                     name="email"
                                     value="{{$midwife->email}}"
                                 >
+                                @error('email')
+                                <small class="invalid-feedback">
+                                    {{ $message }}
+                                </small>
+                                @enderror
                             </div>
 
                         </div>
@@ -90,36 +105,51 @@
                                 <label for="phone" class="col-form-label">Phone</label>
                                 <input
                                     type="text"
-                                    class="form-control form-control-alt"
+                                    class="form-control form-control-alt @error('phone') is-invalid @enderror"
                                     id="phone"
                                     placeholder="Enter Phone"
                                     name="phone"
                                     value="{{$midwife->phone}}"
                                 >
+                                @error('phone')
+                                <small class="invalid-feedback">
+                                    {{ $message }}
+                                </small>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="nic" class="col-form-label">NIC No</label>
                                 <input
                                     type="text"
-                                    class="form-control form-control-alt"
+                                    class="form-control form-control-alt @error('nic') is-invalid @enderror"
                                     id="nic"
                                     placeholder="Enter NIC No"
                                     name="nic"
                                     value="{{$midwife->nic}}"
                                 >
+                                @error('nic')
+                                <small class="invalid-feedback">
+                                    {{ $message }}
+                                </small>
+                                @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="cases" class="col-form-label">Assigned Cases</label>
                                 <input
                                     type="text"
-                                    class="form-control form-control-alt"
+                                    class="form-control form-control-alt @error('cases') is-invalid @enderror"
                                     id="cases"
                                     placeholder="Enter Assigned Cases"
                                     name="cases"
                                     value="{{$midwife->cases}}"
                                 >
+                                @error('cases')
+                                <small class="invalid-feedback">
+                                    {{ $message }}
+                                </small>
+                                @enderror
                             </div>
 
                         </div>
