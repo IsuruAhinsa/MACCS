cb3c7b32796896164d61b96337d01178bc79845a
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-20 20:38:19 +0530

    sidebar.blade.php modified

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -44,6 +44,25 @@
                     </ul>
                 </li>
 
+                <li class="nav-main-item">
+                    <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                        <i class="nav-main-link-icon si si-user-following"></i>
+                        <span class="nav-main-link-name">Admins</span>
+                    </a>
+                    <ul class="nav-main-submenu">
+                        <li class="nav-main-item">
+                            <a class="nav-main-link" href="{{ route('admin.admins.index') }}">
+                                <span class="nav-main-link-name">All Admins</span>
+                            </a>
+                        </li>
+                        <li class="nav-main-item">
+                            <a class="nav-main-link" href="{{ route('admin.admins.create') }}">
+                                <span class="nav-main-link-name">Create Admin</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
             @elseif(Auth::guard('midwife')->check())
 
                 <li class="nav-main-item">
