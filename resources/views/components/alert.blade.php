7e94b03435a565c86b8fc087b158b617d94b8640
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-20 09:47:06 +0530

    Alert Component Created

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,15 @@
+<div class="alert alert-{{ $type }} alert-dismissable" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    <h3 class="alert-heading font-w300 my-2">
+        @if($type == 'success')
+            {{ ucfirst($type) }}!
+        @elseif($type == 'danger')
+            {{ ucfirst('error') }}!
+        @elseif($type == 'warning')
+            {{ ucfirst($type) }}!
+        @endif
+    </h3>
+    <p class="mb-0">{{ $message }}</p>
+</div>
