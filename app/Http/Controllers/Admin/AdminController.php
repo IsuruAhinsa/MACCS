0b5298f1840303870b63fac60e3ee5ec5daf999f
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-22 08:03:21 +0530

    All admin display started

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,11 +16,11 @@ class AdminController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $records = Admin::all();
+        return view('admin.admin.index')->with(compact('records'));
     }
 
     /**
