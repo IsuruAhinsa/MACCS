bd4c1f3b69385d37f7ca885d01aefd34dc665954
ChamathkaDissanayaka <chamathka.dissa@gmail.com>
2021-07-22 22:16:53 +0530

    UserAccountCreated.php and created.blade.php created

---
+++ app/Mail/UserAccountCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserAccountCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user, $password, $url;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+        $this->url = route('user.login');
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.accounts.user.created');
+    }
+}
