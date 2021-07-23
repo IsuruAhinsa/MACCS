e1e70bb002a49a8406554117f44de77a4baad477
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-23 14:46:33 +0530

    Create child function started

---
+++ database/migrations/2021_07_23_090838_create_children_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChildrenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->date('birthday')->nullable();
+            $table->date('date_of_registration')->nullable();
+            $table->float('birth_weight')->nullable();
+            $table->string('health_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+}
