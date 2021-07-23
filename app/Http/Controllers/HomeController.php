756bbfa5b19c71c3e9b697d97955291b3d15c0f1
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-23 19:53:30 +0530

    Parent Update password done & dusted

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Child;
+use App\Http\Requests\UpdatePasswordRequest;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -39,9 +42,21 @@ class HomeController extends Controller
 
     }
 
-    public function updatePassword(Request $request)
+    public function updatePassword(UpdatePasswordRequest $request)
     {
-
+        // check if the hash passwords are match
+        if (Hash::check($request->current_password, Auth::user()->password)) {
+            // hashing the new password
+            $hashed_password = Hash::make($request->password_confirmation);
+            // update password
+            User::where('id', Auth::id())->update([
+                'password' => $hashed_password,
+            ]);
+            // return redirect to back with success message
+            return back()->with('success', 'Your Password has been updated!');
+        } else {
+            return back()->withErrors(['current_password' => 'Your current password is not valid. Please enter your valid password.']);
+        }
     }
 
     protected function getChildren()
