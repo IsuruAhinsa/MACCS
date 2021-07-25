019422af454338715818577525c2b19ba3b22a79
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-25 11:39:02 +0530

    Delete height functions done with livewire

--- app/Http/Livewire/Modal/Height/Create.php
+++ app/Http/Livewire/Modal/Height/Create.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Modal\Height;
 
+use App\Height;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
@@ -10,6 +11,12 @@ class Create extends Component
     public $child_id;
 
     protected $listeners = ['refreshCreateHeightParent' => '$refresh'];
+
+    public function delete($id)
+    {
+        Height::find($id)->delete();
+        $this->emit('refreshCreateHeightParent');
+    }
 
     public function render()
     {
