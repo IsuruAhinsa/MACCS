3d736cf5191c5e3c7cb4b663dbe9b0da5adbedef
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-27 20:53:17 +0530

    parent appointments designed

--- resources/views/users/appointments/index.blade.php
+++ resources/views/users/appointments/index.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
 
-@push('css')
-    <link rel="stylesheet" href="{{ asset('assets/js/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') }}">
-@endpush
 
 @section('content')
 
@@ -10,15 +7,13 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill h3 my-2">
-                   Create Appointment
+                    Table Helpers <small class="d-block d-sm-inline-block mt-2 mt-sm-0 font-size-base font-w400 text-muted">Custom functionality to further enrich your tables.</small>
                 </h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-alt">
-                        <li class="breadcrumb-item">
-                            <a class="link-fx" href="{{ url('/') }}">Dashboard</a>
-                        </li>
+                        <li class="breadcrumb-item">Tables</li>
                         <li class="breadcrumb-item" aria-current="page">
-                            Create Appointment
+                            <a class="link-fx" href="">Helpers</a>
                         </li>
                     </ol>
                 </nav>
@@ -26,115 +21,55 @@
         </div>
     </div>
 
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-sm-7">
-                <div class="content">
+    <div class="content">
+        <div class="block">
+            <div class="block-header">
+                <h3 class="block-title">Checkable Table</h3>
+            </div>
+            <div class="block-content">
 
-                    @if(Session::has('success'))
-                        <x-Alert type="success" :message="Session::get('success')"></x-Alert>
-                    @endif
-
-
-                    <div class="block block-themed">
-                        <div class="block-header bg-smooth">
-                            <h3 class="block-title">
-                                Add Child
-                            </h3>
-                        </div>
-                        <div class="block-content">
-
-                            <form class="form-horizontal p-4" action="{{ route('appointments.store') }}" method="POST">
-
-                                @csrf
-
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label text-right" for="date">Date</label>
-                                    <div class="col-sm-9">
-                                        <input
-                                            type="text"
-                                            class="js-datepicker form-control form-control-alt @error('date') is-invalid @enderror"
-                                            id="date"
-                                            name="date"
-                                            data-week-start="1"
-                                            data-autoclose="true"
-                                            data-today-highlight="true"
-                                            data-date-format="yyyy-mm-dd"
-                                            placeholder="yyyy-mm-dd"
-                                            value="{{ old('date') }}"
-                                        >
-                                        @error('date')
-                                        <small class="invalid-feedback">
-                                            {{ $message }}
-                                        </small>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label text-right" for="time">Time</label>
-                                    <div class="col-sm-9">
-                                        <input
-                                            type="time"
-                                            class="form-control form-control-alt @error('time') is-invalid @enderror"
-                                            name="time"
-                                            id="time"
-                                            value="{{ old('time') }}"
-                                        >
-                                        @error('time')
-                                        <small class="invalid-feedback">
-                                            {{ $message }}
-                                        </small>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label text-right" for="venue">Venue</label>
-                                    <div class="col-sm-9">
-                                        <input
-                                            type="text"
-                                            class="form-control form-control-alt @error('venue') is-invalid @enderror"
-                                            id="venue"
-                                            placeholder="Enter Venue"
-                                            name="venue"
-                                            value="{{ old('venue') }}"
-                                        >
-                                        @error('venue')
-                                        <small class="invalid-feedback">
-                                            {{ $message }}
-                                        </small>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="form-group row">
-                                    <label class="col-sm-3 col-form-label text-right" for="notes">Notes</label>
-                                    <div class="col-sm-9">
-                                        <textarea
-                                            name="notes"
-                                            id="notes"
-                                            class="form-control form-control-alt"
-                                            placeholder="Drop a note"
-                                        ></textarea>
-                                        @error('notes')
-                                        <small class="invalid-feedback">
-                                            {{ $message }}
-                                        </small>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <x-SubmitButton :btnText="'Create Appointment'" :cancelBtnRoute="url('/')"></x-SubmitButton>
-                                </div>
-
-                            </form>
-
-                        </div>
-                    </div>
-
-                </div>
+                <table class="js-table-checkable table table-hover table-vcenter">
+                    <thead>
+                    <tr>
+                        <th class="text-center" style="width: 70px;">
+                            <div class="custom-control custom-checkbox custom-control-primary d-inline-block">
+                                <input type="checkbox" class="custom-control-input" id="check-all" name="check-all">
+                                <label class="custom-control-label" for="check-all"></label>
+                            </div>
+                        </th>
+                        <th style="width: 30%;">Midwife</th>
+                        <th class="d-none d-sm-table-cell" style="width: 5%;">Date</th>
+                        <th class="d-none d-sm-table-cell" style="width: 5%;">Time</th>
+                        <th class="d-none d-sm-table-cell" style="width: 20%;">Where</th>
+                        <th class="d-none d-sm-table-cell" style="width: 20%;">Status</th>
+                        <th class="d-none d-sm-table-cell" style="width: 20%;">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="text-center">
+                            <div class="custom-control custom-checkbox custom-control-primary d-inline-block">
+                                <input type="checkbox" class="custom-control-input" id="row_1" name="row_1">
+                                <label class="custom-control-label" for="row_1"></label>
+                            </div>
+                        </td>
+                        <td class="font-size-sm">
+                            <p class="font-w600 mb-1">
+                                <a href="be_pages_generic_profile.html">Sara Fields</a>
+                            </p>
+                            <p class="text-muted mb-0">
+                                Customer details and further information
+                            </p>
+                        </td>
+                        <td class="d-none d-sm-table-cell">
+                            <span class="badge badge-danger">Disabled</span>
+                        </td>
+                        <td class="d-none d-sm-table-cell">
+                            <em class="font-size-sm text-muted">November 17, 2018 08:35</em>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
@@ -142,6 +77,5 @@
 @endsection
 
 @push('js')
-    <script src="{{ asset('assets/js/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
-    <script>jQuery(function(){ One.helpers(['datepicker']); });</script>
+    <script>jQuery(function(){ One.helpers(['table-tools-checkable']); });</script>
 @endpush
