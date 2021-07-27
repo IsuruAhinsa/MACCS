95619a271676110a98340cfe964602a8fa6e26f6
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-27 12:15:21 +0530

    create appointment view designed

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -20,22 +20,19 @@ class AppointmentController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('users.appointments.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        return $request->all();
     }
 
     /**
