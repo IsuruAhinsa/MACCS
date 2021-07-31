3b960692df741f63287bc858be98f2244afcdcdf
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-07-31 18:14:12 +0530

    Dashboard Modified

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -21,46 +21,44 @@
             </div>
         </div>
     </div>
-    <!-- END Hero -->
 
-    <!-- Page Content -->
     <div class="content content-narrow">
-        <!-- Stats -->
+
         <div class="row">
             <div class="col-6 col-md-3 col-lg-6 col-xl-3">
                 <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
                     <div class="block-content block-content-full">
                         <div class="font-size-sm font-w600 text-uppercase text-muted">Children</div>
-                        <div class="font-size-h2 font-w400 text-dark">580</div>
+                        <div class="font-size-h2 font-w400 text-dark">{{ \App\Child::count() }}</div>
                     </div>
                 </a>
             </div>
             <div class="col-6 col-md-3 col-lg-6 col-xl-3">
-                <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                <a class="block block-rounded block-link-pop border-left border-warning border-4x" href="javascript:void(0)">
                     <div class="block-content block-content-full">
                         <div class="font-size-sm font-w600 text-uppercase text-muted">Midwives</div>
-                        <div class="font-size-h2 font-w400 text-dark">150</div>
+                        <div class="font-size-h2 font-w400 text-dark">{{ \App\Midwife::count() }}</div>
                     </div>
                 </a>
             </div>
             <div class="col-6 col-md-3 col-lg-6 col-xl-3">
-                <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                <a class="block block-rounded block-link-pop border-left border-success border-4x" href="javascript:void(0)">
                     <div class="block-content block-content-full">
                         <div class="font-size-sm font-w600 text-uppercase text-muted">Parents</div>
-                        <div class="font-size-h2 font-w400 text-dark">200</div>
+                        <div class="font-size-h2 font-w400 text-dark">{{ \App\User::count() }}</div>
                     </div>
                 </a>
             </div>
             <div class="col-6 col-md-3 col-lg-6 col-xl-3">
-                <a class="block block-rounded block-link-pop border-left border-primary border-4x" href="javascript:void(0)">
+                <a class="block block-rounded block-link-pop border-left border-danger border-4x" href="javascript:void(0)">
                     <div class="block-content block-content-full">
-                        <div class="font-size-sm font-w600 text-uppercase text-muted">Users</div>
-                        <div class="font-size-h2 font-w400 text-dark">21</div>
+                        <div class="font-size-sm font-w600 text-uppercase text-muted">Admins</div>
+                        <div class="font-size-h2 font-w400 text-dark">{{ \App\Admin::count() }}</div>
                     </div>
                 </a>
             </div>
         </div>
-        <!-- END Stats -->
+
     </div>
 
 @endsection
