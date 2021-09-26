a077fa321cee86c0e8b7f134b0bb5e7dc2fd9f61
Isuru Ahinsa <isurujayawickramat@gmail.com>
2021-09-26 18:01:06 +0530

    fixed birth validation

--- app/Http/Requests/SaveChildRequest.php
+++ app/Http/Requests/SaveChildRequest.php
@@ -29,8 +29,15 @@ class SaveChildRequest extends FormRequest
             'last_name' => ['nullable', 'string', 'max:70'],
             'birthday'=> ['required', 'date', 'before_or_equal: today', 'after_or_equal: today - 5 years'],
             'date_of_registration'=> ['nullable', 'date'],
-            'birth_weight'=> ['numeric', 'between:0,99.99'],
+            'birth_weight'=> ['numeric', 'between:0.0,5.0'],
             'health_status'=> ['required', 'string'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'birth_weight.between' => 'Birth weight must be between 0.0 to 5.0'
         ];
     }
 }
